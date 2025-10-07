6cbc801ce4d73a2092a07c4883eb64b46034c8d8
Stan Hurks <stan@hurks.digital>
2025-10-07 05:37:28 +0000

    This is a patch of previous commit.

--- scripts/stream.ts
+++ scripts/stream.ts
@@ -4,7 +4,7 @@ import Bot from '../src/bot';
 import { ClanApplication } from '../src/entities/clan-application';
 
 const nms = new NodeMediaServer({
-  logType: 0,
+  logType: 3,
   rtmp: {
     port: 8000,
     chunk_size: 60000,
@@ -18,7 +18,7 @@ const nms = new NodeMediaServer({
     mediaroot: path.join(__dirname, '../assets/livestream'),
   },
   trans: {
-    ffmpeg: '/usr/bin/ffmpeg', // make sure ffmpeg is in PATH
+    ffmpeg: '/usr/bin/ffmpeg',
     tasks: [
       {
         app: 'live',
@@ -34,22 +34,22 @@ const nms = new NodeMediaServer({
 Bot.initializeDataSource().then(() => {
   nms.run();
 
-  nms.on('prePublish', async (id, path, args) => {
-    const streamKey = path.split('/').pop();
+  nms.on('prePublish', async (id, streamPath, args) => {
+    const streamKey = streamPath.split('/').pop();
     const repo = Bot.dataSource.getRepository(ClanApplication);
     const app = await repo.findOne({ where: { streamKey } });
 
     if (!app) {
-    //   console.log(`[NodeMediaServer] Stream key ${streamKey} not found. Rejecting...`);
+      console.log(`[NodeMediaServer] Invalid stream key "${streamKey}". Rejecting...`);
       const session = nms.getSession(id);
-      (session as any).reject('Invalid stream key. Please set one using /set-stream-key in your ticket in our discord.');
+		(session as any).reject('Invalid stream key. Please use /set-stream-key command.');
       return;
     }
 
-    console.log(`[NodeMediaServer] Stream ${streamKey} is starting...`);
+    console.log(`[NodeMediaServer] Stream "${streamKey}" is starting...`);
   });
 
   nms.on('donePublish', (id, streamPath, args) => {
-    // console.log(`[NodeMediaServer] Stream ${streamPath} has ended.`);
+    console.log(`[NodeMediaServer] Stream "${streamPath}" has ended.`);
   });
-})
+});
